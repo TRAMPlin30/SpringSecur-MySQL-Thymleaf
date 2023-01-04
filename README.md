1c5cb65a701598a6ab5b48fd05bcf132a47b5cd7
TRAMPlin30 <vicodin28@gmail.com>
2023-01-04 20:13:57 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Customizable login form and registration form with adding users
+to the database (my Spring Security example).
+Using DaoAuthenticationProvider().
--- src/main/java/geekBrains/security/configs/SecurityConfig.java
+++ src/main/java/geekBrains/security/configs/SecurityConfig.java
@@ -65,4 +65,4 @@ public class SecurityConfig {
     }
 }
 
-//Customizable login form and registration form with adding users to the database (my Spring Security example). Using DaoAuthenticationProvider().
+
